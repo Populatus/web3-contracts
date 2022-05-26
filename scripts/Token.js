bc4db7d4169eefceced92e96cd5f59c556fbcbf6
cxygg <cxy19940220@163.com>
2022-05-26 17:47:11 +0800

    create contract

---
+++ scripts/Token.js
@@ -0,0 +1,45 @@
+const { ethers } = require("hardhat")
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+
+  await token.deployed()
+  console.log("Token address:", token.address);
+  saveFrontendFiles(token)
+}
+
+function saveFrontendFiles(token) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../frontend/src/contracts";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + "/contract-address.json",
+    JSON.stringify({ Token: token.address }, undefined, 2)
+  );
+
+  const TokenArtifact = artifacts.readArtifactSync("Token");
+
+  fs.writeFileSync(
+    contractsDir + "/Token.json",
+    JSON.stringify(TokenArtifact, null, 2)
+  );
+}
+
+
+main().then(() => {
+  process.exit(0)
+}).catch(err => {
+  console.error(err)
+  process.exit(1)
+})
